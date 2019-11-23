ce316b16d36e405fee857ef66a2f10918e645e27
mrDevGo <mrdevgo@gmail.com>
2019-11-23 10:09:31 +0300

    if,else

--- app/src/main/java/com/example/Game.kt
+++ app/src/main/java/com/example/Game.kt
@@ -3,63 +3,37 @@ package com.example
 
 fun main() {
 
-    var playerName = "Алекс"
-    var playerAge = 30
+    val world = "Актия"
+    val playerName = "Алекс"
+    val playerAge = 30
     var playerLives = 3
     var playerMoney = 6.2
-    var playerPower = 5
-    val world = "Актия"
     var isSavedGame = true
-    var playerRice = "Human"
-
+    var playerRice = "Humans"
 
 
     if (isSavedGame) {
         println("LoadGame...")
         println("StartGame..")
+        playerLives = 3
     } else {
         println("StartNewGame")
     }
 
 
-    println("Мир $world")
-
-
-    if (playerRice == "Human") {
-        println("Раса игрока человек")
-        playerName = "Alex"
-        playerAge = 30
-        playerLives = 6
-        playerMoney = 20.5
-        playerPower = 10
-        println("Игрок $playerName готов")
-        println("Возраст: $playerAge")
-        println("Жизни: $playerLives")
-        println("Деньги: $playerMoney")
-
+    if (playerRice == "Humans") {
+        println("Раса игрока Люди")
     } else if (playerRice == "Ork") {
-        println("Раса игрока орк")
-        playerName = "Jodi"
-        playerAge = 120
-        playerLives = 5
-        playerMoney = 2.3
-        playerPower = 20
-        println("Игрок $playerName готов")
-        println("Возраст: $playerAge")
-        println("Жизни: $playerLives")
-        println("Деньги: $playerMoney")
-    } else {
-        println("Раса игрока дракон")
-        playerName = "Tran"
-        playerAge = 650
-        playerLives = 2
-        playerMoney = 9.0
-        playerPower = 60
-        println("Игрок $playerName готов")
-        println("Возраст: $playerAge")
-        println("Жизни: $playerLives")
-        println("Деньги: $playerMoney")
+        println("Раса игрока Орки")
+    } else if (playerRice == "Demons") {
+        println("Раса игрока Демоны")
     }
+
+    println("Мир $world")
+    println("Игрок $playerName готов")
+    println("Возраст: $playerAge")
+    println("Жизни: $playerLives")
+    println("Деньги: $playerMoney")
 
 
 }
